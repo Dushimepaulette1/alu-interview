f3b46dc9df97e6daaf6735c561d8e9a3343d1b9a
Dushimepaulette1 <p.dushime1@alustudent.com>
2025-02-22 21:04:56 +0000

    pascal triangle

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This prints out the pascal's traingle"""
+
+def pascal_traingle(n):
+ """This prints out the pascal's traingle"""
+  if n <= 0:
+    return []
+  triangle = [[1]]# Initialize with the first row
+  for i in range(1, n):
+    """Executes when n above 0"""
+    row = [1]
+    """Compute the inner values by summing adjacent values from the previous row"""
+    for j in range(1, i):
+        row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+    # End the row with 1
+    row.append(1)
+    triangle.append(row)
+
+  return triangle
