46d005b95ccb4b71a6b1f6680f93eff3f0d18468
Dushimepaulette1 <p.dushime1@alustudent.com>
2025-02-22 21:11:06 +0000

    pascal triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""This prints out the pascal's traingle"""
+"""This module prints Pascal's triangle"""
 
-def pascal_traingle(n):
- """This prints out the pascal's traingle"""
-  if n <= 0:
-    return []
-  triangle = [[1]]# Initialize with the first row
-  for i in range(1, n):
-    """Executes when n above 0"""
-    row = [1]
-    """Compute the inner values by summing adjacent values from the previous row"""
-    for j in range(1, i):
-        row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-    # End the row with 1
-    row.append(1)
-    triangle.append(row)
+def pascal_triangle(n):
+    """Returns a list of lists representing Pascal's triangle of size n."""
+    if n <= 0:
+        return []
+    
+    triangle = [[1]]  # Initialize with the first row
+    
+    for i in range(1, n):
+        row = [1]  # Start each row with 1
+        
+        # Compute the inner values by summing adjacent values from the previous row
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        
+        row.append(1)  # End each row with 1
+        triangle.append(row)  # Add the computed row to the triangle
+    
+    return triangle
 
-  return triangle
